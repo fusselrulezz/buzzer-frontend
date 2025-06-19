604ebe5d07dbed1835f8365abcfe555a33a4f93d
fusselrulezz <ronnyebert@outlook.com>
2025-06-19 22:02:29 +0200

    ADD: Set up dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
-import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:buzzer/app/app.bottomsheets.dart';
 import 'package:buzzer/app/app.dialogs.dart';
 import 'package:buzzer/app/app.locator.dart';
 import 'package:buzzer/app/app.router.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+
   setPathUrlStrategy();
   await setupLocator(stackedRouter: stackedRouter);
+
   setupDialogUi();
   setupBottomSheetUi();
+
   runApp(const MainApp());
 }
 
@@ -21,14 +27,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveApp(
-      builder: (_) => MaterialApp.router(
-        routerDelegate: stackedRouter.delegate(),
-        routeInformationParser: stackedRouter.defaultRouteParser(),
-      ),
-    ).animate().fadeIn(
-          delay: const Duration(milliseconds: 50),
-          duration: const Duration(milliseconds: 400),
-        );
+    const defaultLocale = Locale("en");
+
+    return EasyLocalization(
+      supportedLocales: const [
+        defaultLocale,
+      ],
+      fallbackLocale: defaultLocale,
+      path: "assets/lang",
+      child: ResponsiveApp(
+        builder: (context) => ShadcnApp.router(
+          debugShowCheckedModeBanner: false,
+          routerDelegate: stackedRouter.delegate(),
+          routeInformationParser: stackedRouter.defaultRouteParser(),
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          theme: ThemeData(
+            colorScheme: ColorSchemes.lightGray(),
+            radius: 0.5,
+          ),
+        ),
+      ).animate().fadeIn(
+            delay: const Duration(milliseconds: 50),
+            duration: const Duration(milliseconds: 400),
+          ),
+    );
   }
 }
