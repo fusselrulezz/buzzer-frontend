389d399f3f4f17fb0be3bb0774f0a52e9455666c
fusselrulezz <ronnyebert@outlook.com>
2025-06-21 15:10:13 +0200

    ADD: Scaffold home view

--- lib/ui/views/home/home_view.desktop.dart
+++ lib/ui/views/home/home_view.desktop.dart
@@ -1,8 +1,7 @@
-import 'package:buzzer/ui/common/app_colors.dart';
-import 'package:buzzer/ui/common/app_constants.dart';
-import 'package:buzzer/ui/common/ui_helpers.dart';
-import 'package:flutter/material.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:stacked/stacked.dart';
+
+import 'package:buzzer/ui/common/ui_helpers.dart';
 
 import 'home_viewmodel.dart';
 
@@ -11,57 +10,70 @@ class HomeViewDesktop extends ViewModelWidget<HomeViewModel> {
 
   @override
   Widget build(BuildContext context, HomeViewModel viewModel) {
+    const horizontalPadding = 64.0;
+
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: kdDesktopMaxContentWidth,
-          height: kdDesktopMaxContentHeight,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              verticalSpaceLarge,
-              Column(
-                children: [
-                  const Text(
-                    'Hello, DESKTOP UI!',
-                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
-                  ),
-                  verticalSpaceMedium,
-                  MaterialButton(
-                    color: Colors.black,
-                    onPressed: viewModel.incrementCounter,
-                    child: Text(
-                      viewModel.counterLabel,
-                      style: const TextStyle(color: Colors.white),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 32.0,
+              horizontal: horizontalPadding,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Buzzer").h1,
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Login").h3,
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  MaterialButton(
-                    color: kcDarkGreyColor,
-                    onPressed: viewModel.showDialog,
-                    child: const Text(
-                      'Show Dialog',
-                      style: TextStyle(color: Colors.white),
+                    horizontalSpaceSmall,
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Register").h3,
                     ),
-                  ),
-                  MaterialButton(
-                    color: kcDarkGreyColor,
-                    onPressed: viewModel.showBottomSheet,
-                    child: const Text(
-                      'Show Bottom Sheet',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 32.0,
+                horizontal: horizontalPadding,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(child: const Text("Create a room").h3),
+                        verticalSpaceMedium,
+                        const Text(
+                                "Create a room for your game or event, and invite others to join.")
+                            .base,
+                      ],
+                    ),
+                  ),
+                  const VerticalDivider(),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(child: const Text("Join a room").h3),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
