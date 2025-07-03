27778036c08d2908bbbe9bfd97f2dc1692bf16ea
fusselrulezz <ronnyebert@outlook.com>
2025-07-03 20:50:16 +0200

    ADD: Enhance localization support by adding locale names and fallback handling

--- lib/ui/widgets/common/settings_dialog/settings_dialog_model.dart
+++ lib/ui/widgets/common/settings_dialog/settings_dialog_model.dart
@@ -1,11 +1,15 @@
 import "package:adaptive_theme/adaptive_theme.dart";
-import "package:buzzer/ui/common/shadcn_adaptive_theme.dart";
 import "package:easy_localization/easy_localization.dart";
 import "package:flutter/widgets.dart";
+import "package:logger/logger.dart";
 
+import "package:buzzer/app/app_logger.dart";
 import "package:buzzer/mvvm/base_view_models.dart";
+import "package:buzzer/ui/common/shadcn_adaptive_theme.dart";
 
 class SettingsDialogModel extends BaseViewModel {
+  final Logger _logger = getLogger("SettingsDialogModel");
+
   final List<AdaptiveThemeMode> themeModes = [
     AdaptiveThemeMode.light,
     AdaptiveThemeMode.dark,
@@ -34,15 +38,43 @@ class SettingsDialogModel extends BaseViewModel {
   }
 
   List<Locale> getSupportedLocales(BuildContext context) {
-    return EasyLocalization.of(context)?.supportedLocales ?? [];
+    final easyLocalization = EasyLocalization.of(context);
+
+    if (easyLocalization == null) {
+      // This should really not ever happen, but if it does, we return English
+      // as a fallback locale to prevent crashes.
+      _logger.w("Localization is not initialized, returning default locales.");
+      return [const Locale("en")];
+    }
+
+    return easyLocalization.supportedLocales;
   }
 
   Locale locale(BuildContext context) {
-    return EasyLocalization.of(context)?.locale ?? const Locale("en");
+    final easyLocalization = EasyLocalization.of(context);
+
+    if (easyLocalization == null) {
+      // This should really not ever happen, but if it does, we return English
+      // as a fallback locale to prevent crashes.
+      _logger.w("Localization is not initialized, returning default locale.");
+      return const Locale("en");
+    }
+
+    return easyLocalization.locale;
   }
 
   String localeName(Locale locale) {
-    return locale.languageCode;
+    return _localeNameTranslationKey(locale).tr();
+  }
+
+  String _localeNameTranslationKey(Locale locale) {
+    StringBuffer sb = StringBuffer();
+
+    sb.write("locale.names.");
+    sb.write(locale.toLanguageTag());
+    sb.write(".name");
+
+    return sb.toString();
   }
 
   void onLocaleChanged(BuildContext context, Locale? value) {
