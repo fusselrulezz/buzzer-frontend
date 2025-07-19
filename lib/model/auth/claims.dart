014e2b8272217a2ed924e04df46ef153d762bce2
fusselrulezz <ronnyebert@outlook.com>
2025-07-19 14:22:23 +0200

    FIX: Parse claims when creating an Identity instance

--- lib/model/auth/claims.dart
+++ lib/model/auth/claims.dart
@@ -1,5 +1,8 @@
-import "claim.dart";
+import "package:collection/collection.dart";
 
 /// Represents a collection of claims, which are key-value pairs used for
 /// authentication and authorization purposes.
-extension Claims on List<Claim> {}
+class Claims extends DelegatingMap<String, dynamic> {
+  /// Initializes a new [Claims] instance with the provided map of claims.
+  Claims(super.base);
+}
--- lib/model/identity.dart
+++ lib/model/identity.dart
@@ -1,3 +1,7 @@
+import "dart:convert";
+
+import "package:buzzer/model/auth/claims.dart";
+
 /// Stores the identity of the user, including access and refresh tokens.
 class Identity {
   /// The access token used for authentication.
@@ -6,14 +10,68 @@ class Identity {
   /// The refresh token used to obtain a new access token.
   final String refreshToken;
 
-  /// Initializes a new [SmartIdentity] instance with the provided access and refresh tokens.
-  Identity._({required this.accessToken, required this.refreshToken});
+  /// The claims associated with the identity.
+  final Claims claims;
+
+  /// Initializes a new [Identity] instance with the provided access and refresh tokens.
+  Identity._({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.claims,
+  });
 
   /// Creates a new [Identity] instance from the provided access and refresh tokens.
   static Identity create({
     required String accessToken,
     required String refreshToken,
   }) {
-    return Identity._(accessToken: accessToken, refreshToken: refreshToken);
+    final claimsMap = _parseJwt(accessToken) ?? {};
+
+    return Identity._(
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      claims: Claims(claimsMap),
+    );
+  }
+
+  static Map<String, dynamic>? _parseJwt(String token) {
+    final parts = token.split(".");
+
+    if (parts.length != 3) {
+      return null;
+    }
+
+    final payload = _decodeBase64(parts[1]);
+
+    if (payload == null) {
+      return null;
+    }
+
+    final payloadMap = json.decode(payload);
+
+    if (payloadMap is! Map<String, dynamic>) {
+      return null;
+    }
+
+    return payloadMap;
+  }
+
+  static String? _decodeBase64(String str) {
+    var output = str.replaceAll("-", "+").replaceAll("_", "/");
+
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += "==";
+        break;
+      case 3:
+        output += "=";
+        break;
+      default:
+        return null; // Invalid base64 string
+    }
+
+    return utf8.decode(base64Url.decode(output));
   }
 }
