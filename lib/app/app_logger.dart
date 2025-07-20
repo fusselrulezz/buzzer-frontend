fa1cbf23cb3b867d1267cf4aae05ddaa7ff242b0
fusselrulezz <ronnyebert@outlook.com>
2025-07-20 21:51:14 +0200

    ADD: Implement custom logger with detailed log formatting and error handling

--- lib/app/app_logger.dart
+++ lib/app/app_logger.dart
@@ -1,6 +1,109 @@
+import "dart:convert";
+
 import "package:logger/logger.dart";
 
 /// Returns the a default [Logger] instance for the application.
 Logger getLogger(String name) {
-  return Logger(printer: SimplePrinter(colors: true, printTime: true));
+  return Logger(printer: _BuzzerLogPrinter(name: name));
+}
+
+class _BuzzerLogPrinter extends LogPrinter {
+  /// The name of the logger, used for identification in log messages.
+  final String name;
+
+  static const int _maxNameLength = 20;
+
+  /// Creates a new instance of [_BuzzerLogPrinter] with the specified [name].
+  _BuzzerLogPrinter({required this.name});
+
+  @override
+  List<String> log(LogEvent event) {
+    return _lines(event).toList();
+  }
+
+  Iterable<String> _lines(LogEvent event) sync* {
+    final buffer = StringBuffer();
+
+    // Log level and color
+
+    final prefix = _prefix(event.level);
+    final color = _color(event.level);
+
+    buffer.write("[");
+
+    if (color.color) {
+      buffer.write(color(prefix));
+    } else {
+      buffer.write(prefix);
+    }
+
+    buffer.write("] ");
+
+    // Time
+
+    final time = event.time.toIso8601String();
+
+    buffer.write("[");
+    buffer.write(time);
+    buffer.write("] ");
+
+    // Logger name
+
+    buffer.write("[");
+    buffer.write(name.padRight(_maxNameLength).substring(0, _maxNameLength));
+    buffer.write("]: ");
+
+    // Message
+
+    final message = _extractMessage(event.message);
+    buffer.write(message);
+
+    // Error and stack trace
+
+    if (event.error != null) {
+      buffer.write("\n");
+      buffer.writeln(event.error.toString());
+
+      if (event.stackTrace != null) {
+        buffer.writeln(event.stackTrace.toString());
+      }
+    }
+
+    yield buffer.toString();
+  }
+
+  String _prefix(Level level) {
+    return switch (level) {
+      Level.trace => "T",
+      Level.debug => "D",
+      Level.info => "I",
+      Level.warning => "W",
+      Level.error => "E",
+      Level.fatal => "F",
+      _ => "",
+    };
+  }
+
+  AnsiColor _color(Level level) {
+    return switch (level) {
+      Level.trace => AnsiColor.fg(AnsiColor.grey(0.5)),
+      Level.debug => const AnsiColor.none(),
+      Level.info => const AnsiColor.fg(12),
+      Level.warning => const AnsiColor.fg(208),
+      Level.error => const AnsiColor.fg(196),
+      Level.fatal => const AnsiColor.fg(199),
+      _ => const AnsiColor.none(),
+    };
+  }
+
+  String _extractMessage(message) {
+    final finalMessage = message is Function ? message() : message;
+
+    if (finalMessage is Map || finalMessage is Iterable) {
+      var encoder = const JsonEncoder.withIndent(null);
+      return encoder.convert(finalMessage);
+    } else {
+      return finalMessage.toString();
+    }
+  }
 }
